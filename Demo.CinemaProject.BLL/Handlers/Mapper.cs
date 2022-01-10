679bdea2c24dd2dfbbe1d2ba7c0bd02f8028be0a
Legrain Samuel <samuel.legrain@bstorm.be>
2022-01-10 12:21:59 +0100

    Add BLL Mapper & FilmService

---
+++ Demo.CinemaProject.BLL/Handlers/Mapper.cs
@@ -0,0 +1,73 @@
+﻿using B = Demo.CinemaProject.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using D = Demo.CinemaProject.DAL.Entities;
+
+namespace Demo.CinemaProject.BLL.Handlers
+{
+    public static class Mapper
+    {
+        public static B.Cinema ToBLL(this D.Cinema entity)
+        {
+            if(entity == null) return null;
+            return new B.Cinema(
+                entity.Id,
+                entity.Nom,
+                entity.Ville
+                );
+        }
+
+        public static D.Cinema ToDAL(this B.Cinema entity)
+        {
+            if(entity==null) return null;
+            return new D.Cinema
+            {
+                Id = entity.Id,
+                Nom = entity.Nom,
+                Ville = entity.Ville
+            };
+        }
+        public static B.Film ToBLL(this D.Film entity)
+        {
+            if (entity == null) return null;
+            return new B.Film(
+                entity.Id,
+                entity.Titre,
+                entity.DateSortie
+                );
+        }
+        public static D.Film ToDAL(this B.Film entity)
+        {
+            if (entity == null) return null;
+            return new D.Film
+            {
+                Id =entity.Id,
+                Titre =entity.Titre,
+                DateSortie = entity.DateSortie
+            };
+        }
+
+        public static B.Diffusion ToBLL(this D.Diffusion entity)
+        {
+            if( entity == null) return null;
+            return new B.Diffusion(
+                entity.Id,
+                entity.DateDiffusion,
+                null,
+                null
+                );
+        }
+
+        public static D.Diffusion ToDAL(this B.Diffusion entity)
+        {
+            if(entity == null) return null;
+            return new D.Diffusion { 
+                Id = entity.Id,
+                DateDiffusion = entity.DateDiffusion,
+                Cinema_Id = entity.Cinema.Id,
+                Film_Id = entity.Film.Id
+            };
+        }
+    }
+}
---
+++ Demo.CinemaProject.BLL/Services/FilmService.cs
@@ -0,0 +1,44 @@
+﻿using Demo.CinemaProject.BLL.Entities;
+using Demo.CinemaProject.BLL.Handlers;
+using Demo.CinemaProject.Common.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.CinemaProject.BLL.Services
+{
+    public class FilmService : IRepository<Film, int>
+    {
+        private readonly IRepository<DAL.Entities.Film,int> _filmRepository;
+
+        public FilmService(IRepository<DAL.Entities.Film, int> repository)
+        {
+            _filmRepository = repository;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Film Get(int tid)
+        {
+            return _filmRepository.Get(tid).ToBLL();
+        }
+
+        public IEnumerable<Film> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Film entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Film entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
